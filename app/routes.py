568a070d44f4b51821fd316dd2eebec8d89b4ac0
Calum Kerr <93798847+Calum-Kerr@users.noreply.github.com>
2024-11-06 14:34:16 +0000

    created a simple 3 page template to get started

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,14 @@
+from flask import render_template, request
 from app import app
 @app.route('/')
-def hello(): return 'hello world!'
+def index():return render_template('index.html')
+
+@app.route('/about')
+def about():return render_template('about.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        pdf = request.files['pdf']
+        return f"Uploaded file: {pdf.filename}"
+    return render_template('upload.html')
