bb8eae28eca8c02a7ba9614b29065eb429005492
Steven Miller <stevenerm88@gmail.com>
2025-04-18 18:57:31 +0100

    Implemented Split PDF functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__, template_folder='../templates')
+
+# Add custom filter
+@app.template_filter('basename')
+def basename_filter(path):
+    return os.path.basename(path)
+
 from app import routes
 from tools.optimise import compress
