d798486c1981ceccdde170ddcbf82939e1a91fe2
SMiller-0 <stevenerm88@gmail.com>
2025-04-19 20:22:31 +0100

    Updated imports

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from tools.convert.jpg_to_pdf import jpg_to_pdf_view
 from tools.optimise.compress import compress_view
 from tools.convert.pdf_to_panoramic import pdf_to_panoramic_view
 from tools.organise.split import split_view
+from tools.organise.merge import merge_view
 
 #------------------------- Main Pages -------------------------
 
@@ -199,9 +200,9 @@ def repair():
 def extract():
     return render_template('pages/organise/extract.html')
 
-@app.route('/pages/organise/merge')
+@app.route('/pages/organise/merge', methods=['GET', 'POST'])
 def merge():
-    return render_template('pages/organise/merge.html')
+    return merge_view()
 
 @app.route('/pages/organise/remove')
 def remove():
