3ed0f45118b03f739c3df846b57d1c18e7028bd5
SMiller-0 <stevenerm88@gmail.com>
2025-04-20 19:19:12 +0100

    Added form data

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import FileField, SelectField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, Length
 
 class ImageToPdfForm(FlaskForm):
+    class Meta:
+        csrf = False  # Disable CSRF protection for this form
+        
     file = FileField('Select Image File', validators=[DataRequired()])
     page_size = SelectField('Page Size', choices=[
         ('a4', 'A4'),
@@ -17,3 +20,14 @@ class ImageToPdfForm(FlaskForm):
         ('landscape', 'Landscape')
     ])
     submit = SubmitField('Convert to PDF')
+
+# Update ProtectPDFForm similarly
+class ProtectPDFForm(FlaskForm):
+    class Meta:
+        csrf = False  # Disable CSRF protection for this form
+        
+    file = FileField('Select PDF File', validators=[DataRequired()])
+    user_password = PasswordField('User Password', validators=[Length(min=1, max=32)])
+    owner_password = PasswordField('Owner Password', validators=[Length(min=1, max=32)])
+    confirm_password = PasswordField('Confirm User Password', 
+                                   validators=[EqualTo('user_password', message='Passwords must match')])
